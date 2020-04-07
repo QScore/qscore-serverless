e8884ce2fd3cda019d7f551ad8b9381ffbc8bd8e
Phil Olson <polson55@gmail.com>
2020-04-06 19:49:57 -0500

    Do some refactoring to make unit testable

--- score/scoreManager.ts
+++ score/scoreManager.ts
@@ -1,30 +1,16 @@
-import * as AWS  from "aws-sdk"
-const userTableName: string = process.env.USERS_TABLE_NAME!
-const eventsTableName = process.env.GEOFENCE_EVENTS_TABLE_NAME!
-const documentClient = new AWS.DynamoDB.DocumentClient()
 const oneDayMillis = (24 * 60 * 60 * 1000)
-const yesterdayMillis = new Date().getTime() - oneDayMillis
 const debug = false
 
-interface Event {
-    readonly timestamp: number
-    readonly eventType: "HOME" | "AWAY"
-}
+export async function calculateScore(userId: string, repository: Repository): Promise<number> {
+    
+    const yesterdayMillis = new Date().getTime() - oneDayMillis
+    const events = await repository.getEventsFrom(userId, yesterdayMillis)
 
-export async function  updateScore(userId: string) {
-    const eventsResult = await getAllEvents(userId)
-    if (!eventsResult.Items) {
-        console.log("Unable to get events")
-        return 0
-    }
-
-    const events = eventsResult.Items as Event[]
     if (debug) console.log("Queried all events: " + JSON.stringify(events.length))
 
     //Handler case where there are no events for this user, this should never happen
     if (events.length == 0) {
         console.log("No events found for this user, automatic 0")
-        await updateUserScore(userId, 0)
         return 0
     }
 
@@ -54,7 +40,7 @@ export async function  updateScore(userId: string) {
     //Handle error case where no events in last 24 hours.
     if (last24HoursEvents.length == 0) {
         console.log("No events in the last 24 hours")
-        return
+        return -1
     }
 
     //Handle edge case where first event in last 24 hours is AWAY.
@@ -103,42 +89,6 @@ export async function  updateScore(userId: string) {
     const finalScore = timeAtHome / oneDayMillis * 100
     if (debug) console.log(`Final score: ${finalScore}`)
 
-    await updateUserScore(userId, finalScore)
     console.log(`Successfully updated score ${finalScore} for user ${userId}`)
     return finalScore
-}
-
-async function updateUserScore(userId: string, score: number) {
-    const userParams: AWS.DynamoDB.DocumentClient.UpdateItemInput = {
-        TableName: userTableName,
-        Key: { id: userId },
-        UpdateExpression: 'set #score = :score',
-        ExpressionAttributeNames: {
-            '#score': 'score'
-        },
-        ExpressionAttributeValues: {
-            ':score': score
-        }
-    }
-
-    return documentClient.update(userParams).promise()
-}
-
-async function getAllEvents(userId: string) {
-    // Query and build score
-    const eventParams: AWS.DynamoDB.DocumentClient.QueryInput = {
-        TableName: eventsTableName,
-        IndexName: "time-index",
-        KeyConditionExpression: '#userId = :userId AND #timestamp >= :yesterdayMillis',
-        ExpressionAttributeNames: {
-            '#userId': "userId",
-            '#timestamp': "timestamp"
-        },
-        ExpressionAttributeValues: {
-            ':userId': userId,
-            ':yesterdayMillis': yesterdayMillis
-        }
-    }
-
-    return documentClient.query(eventParams).promise()
 }
